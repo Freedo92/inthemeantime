994c364dae4fdef7eaab8cbb9c1bf2b9dd3f78b0
Freedo92 <chriscritter1@gmail.com>
2016-08-15 15:29:33 -0400

    Beginnings of JS
    Testing the API onload auth

---
+++ vid.js
@@ -0,0 +1,16 @@
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyDNYoIpIAKuz9A8EoIvAs3wVghpix_cJzI');
+    
+}
