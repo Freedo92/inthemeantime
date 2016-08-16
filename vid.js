f71926ca24a0b87a374e3ae629ecc319ec2e7520
Freedo92 <chriscritter1@gmail.com>
2016-08-16 02:17:43 -0400

    Hopefully this works

--- vid.js
+++ vid.js
@@ -2,20 +2,22 @@ var apiKey = 'AIzaSyDNYoIpIAKuz9A8EoIvAs3wVghpix_cJzI';
 
 // Called automatically when JavaScript client library is loaded.
 function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+	gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+	console.log("Loaded YouTube client");
 }
 
 // Called automatically when YouTube API interface is loaded (see line 9).
 function onYouTubeApiLoad() {
-    gapi.client.setApiKey(apiKey);
+	gapi.client.setApiKey(apiKey);
+	console.log("API key set");
 }
 
 $(function() {
-    var searchField = $('#search-input');
+	var searchField = $('#search-input');
 
-    $('#search-form').submit(function(e) {
-        e.preventDefault();
-    });
+	$('#search-form').submit(function(e) {
+		e.preventDefault();
+	});
 });
 
 function timeToDuration(minutes) {
@@ -29,48 +31,54 @@ function timeToDuration(minutes) {
 }
 
 function RandomWord() {
-    $.ajax({
-        type: "GET",
-        url: "http://randomword.setgetgo.com/get.php",
-        dataType: "jsonp",
-        jsonpCallback: 'search'
-    });
+	$.ajax({
+		type: "GET",
+		url: "http://randomword.setgetgo.com/get.php",
+		dataType: "jsonp",
+		jsonpCallback: 'search'
+	});
+	console.log("Random word received");
 }
 
 var videoDuration = null;
 
 // Look for a video with the specified time.
 function search(data) {
-	 videoDuration = $('#search-input').val()
+	videoDuration = $('#search-input').val()
 	// Create a search.list() API call. TODO: change snippet to id
-    var request = gapi.client.youtube.search.list({
-    	type: 'video',
-        part: 'snippet',
-        q: data.Word,
-        videoEmbeddable: true,
+	var request = gapi.client.youtube.search.list({
+		type: 'video',
+		part: 'id',
+		q: data.Word,
+		videoEmbeddable: true,
 		videoDuration: timeToDuration(videoDuration)
-    });
+	});
 	
 	request.execute(buildVideoList);
+	console.log("Completed search: " + data);
 }
 
 // Called automatically with the response of the YouTube API request.
 function buildVideoList(response) {
-	var videoIds = $.map(response.items, function(item) {
-		return item.snippet.id.videoId;
-	});
+	if ('error' in response) {
+		displayMessage(response.error.message);
+	} else {
+		var videoIds = $.map(response.items, function(item) {
+			return item.id.videoId; // .snippet. was here too
+		});
 
-    var request = gapi.client.youtube.videos.list({
-    	id: videoIds.join(','),
-    	part: 'id,fileDetails'
-    });
+		var request = gapi.client.youtube.videos.list({
+			id: videoIds.join(','),
+			part: 'id,fileDetails'
+		});
 
-    request.execute(showResponse);
+		request.execute(showResponse);
+	}
 }
 
 // Helper function to display JavaScript value on HTML page.
 function showResponse(response) {
-    $.each(response.items, function() {
+	$.each(response.items, function() {
 		if (this.fileDetails.durationMs <= videoDuration+30000 || 
 			this.fileDetails.durationMs >= videoDuration-30000) {
 			console.log(this.id);
